ac84895c0c67e631ab52bf060271067b1e7dbd05
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2018-01-18 14:54:39 +0100

    initial commit

---
+++ SkinCheck/Plane.swift
@@ -0,0 +1,50 @@
+//
+//  Plane.swift
+//  SkinCheck
+//
+//  Created by Bratislav Ljubisic on 1/18/18.
+//  Copyright © 2018 Bratislav Ljubisic. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class Plane: SCNNode {
+
+    var planeGeometry: SCNBox?
+    var anchor: ARPlaneAnchor?
+    
+    init(anchor: ARPlaneAnchor) {
+        super.init()
+        
+        let width = CGFloat(anchor.extent.x)
+        let length = CGFloat(anchor.extent.z)
+        
+        let planeHeight = 0.001 as CGFloat
+        
+        self.planeGeometry = SCNBox(width: width, height: planeHeight, length: length, chamferRadius: 0)
+        
+        let transparentMaterial = SCNMaterial()
+        transparentMaterial.diffuse.contents = UIColor.white.withAlphaComponent(0.0)
+        
+        self.planeGeometry?.materials = [transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial, transparentMaterial]
+        
+        let planeNode = SCNNode(geometry: planeGeometry)
+        planeNode.position = SCNVector3(0, -planeHeight/2.0, 0)
+        
+        self.addChildNode(planeNode)
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateWith(_ anchor: ARPlaneAnchor) {
+        self.planeGeometry?.width = CGFloat(anchor.extent.x)
+        self.planeGeometry?.length = CGFloat(anchor.extent.z)
+        
+        self.position = SCNVector3(anchor.center.x, 0, anchor.center.z)
+    }
+}
--- SkinCheck/ViewController.swift
+++ SkinCheck/ViewController.swift
@@ -14,28 +14,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var dictPlanes = [ARPlaneAnchor : Plane]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.setupScene()
+        
+
+    }
+    
+    func setupScene() {
         // Set the view's delegate
-        sceneView.delegate = self
+        self.sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        self.sceneView.showsStatistics = true
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
-        sceneView.scene = scene
+        self.sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        self.setupARSession()
+
+    }
+    
+    func setupARSession() {
+        
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
+        configuration.planeDetection = .horizontal
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -62,6 +79,43 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        print("--> did add node!")
+        
+        DispatchQueue.main.async {
+            if let planeAnchor = anchor as? ARPlaneAnchor {
+                let plane = Plane(anchor: planeAnchor)
+                
+                node.addChildNode(plane)
+                self.dictPlanes[planeAnchor] = plane
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            if let planeAnchor = anchor as? ARPlaneAnchor {
+                let plane = self.dictPlanes[planeAnchor]
+                
+                plane?.updateWith(planeAnchor)
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        if let planeAnchor = anchor as? ARPlaneAnchor {
+            self.dictPlanes.removeValue(forKey: planeAnchor)
+        }
+    }
+    
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -76,5 +130,38 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    @IBAction func onAddButtonPressed(_ sender: UIButton) {
+        if let position = self.doHitTestOnExistingPlanes() {
+            let node = self.nodeWithPostion(position)
+            self.sceneView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
+    func doHitTestOnExistingPlanes() -> SCNVector3? {
+        let results = sceneView.hitTest(view.center, types: .existingPlaneUsingExtent)
+        
+        if let result = results.first {
+            let hitPos = self.positionFromTransform(result.worldTransform)
+            
+            return hitPos
+        }
+        return nil
+    }
+    
+    func positionFromTransform(_ transform: matrix_float4x4) -> SCNVector3 {
+        return SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+    }
+    
+    func nodeWithPostion(_ position: SCNVector3) -> SCNNode {
+        let sphere = SCNSphere(radius: 0.05)
+        
+        sphere.firstMaterial?.diffuse.contents = UIColor.red
+        sphere.firstMaterial?.isDoubleSided = true
+        
+        let node = SCNNode(geometry: sphere)
+        node.position = position
+        
+        return node
     }
 }
